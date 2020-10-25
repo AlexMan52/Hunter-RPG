343b0e8a2fca51193db9935cd6d85ca39734a4ce
AlexMan52 <alexman5264@gmail.com>
2020-10-25 13:43:57 +0500

    added movement animations

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -8,10 +8,11 @@ public class Mover : MonoBehaviour
 {
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
     void MoveToCursor()
     {
@@ -24,5 +25,11 @@ public class Mover : MonoBehaviour
         }
         else return;
     }
-
+    void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
 }
